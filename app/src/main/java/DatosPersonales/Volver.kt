87eee801631609faf19b66d42a4bcb5f50105730
Sandra <saandris.aramac@gmail.com>
2025-05-03 19:47:25 +0200

    creación pantalla_datos_personales.xml vertical

---
+++ app/src/main/java/DatosPersonales/Volver.kt
@@ -0,0 +1,4 @@
+package DatosPersonales
+
+class Volver {
+}
--- app/src/main/java/PantallaPrincipal/PantallaPrincipal.kt
+++ app/src/main/java/PantallaPrincipal/PantallaPrincipal.kt
@@ -1,4 +1,14 @@
 package PantallaPrincipal
 
-class PantallaPrincipal {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.threadly.R
+
+
+class PantallaPrincipal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pantalla_aa_inicio)
+    }
 }
