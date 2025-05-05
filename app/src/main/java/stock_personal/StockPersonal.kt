747abf2d633d7794e0bd4ac87d8acdc12e62d53e
Olga <olgamacara@gmail.com>
2025-05-05 13:00:24 +0200

    05/05 - 13:00

--- app/src/main/java/stock_personal/StockPersonal.kt
+++ app/src/main/java/stock_personal/StockPersonal.kt
@@ -1,13 +1,15 @@
 package stock_personal
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.threadly.R
 
 class StockPersonal : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(binding.root)
+        enableEdgeToEdge()
+        setContentView(R.layout.stock_aa_principal)
     }
 }
